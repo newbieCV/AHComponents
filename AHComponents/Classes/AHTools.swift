3d1ac3a042259bde42e23e427bafcf23a53c75fa
newbieCV <zhangzongyu00@gmail.com>
2023-04-07 01:49:31 +0800

    example

---
+++ AHComponents/Classes/AHTools.swift
@@ -0,0 +1,20 @@
+//
+//  AHTools.swift
+//  AHComponents
+//
+//  Created by 张宗宇 on 2023/4/7.
+//
+
+import Foundation
+import UIKit
+
+struct kScreen {
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+}
+
+class AHTools {
+    func test() {
+        print("test success!")
+    }
+}
