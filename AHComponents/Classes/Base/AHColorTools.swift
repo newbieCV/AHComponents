24403b010557e9207a82fc8480923ea7a779440b
zhangzongyu@bytedance.com <zhangzongyu@bytedance.com>
2023-05-16 11:27:54 +0800

    颜色组件扩充

--- AHComponents/Classes/Base/AHColorTools.swift
+++ AHComponents/Classes/Base/AHColorTools.swift
@@ -8,8 +8,23 @@
 import Foundation
 import UIKit
 
-class AHColorTools: UIColor {
-    // 寻找podspec中颜色资源
+public class AHColorTools: UIColor {
+    /// 颜色转纯色图片
+    public class func convertImage(color: UIColor, size: CGSize) -> UIImage {
+        var img: UIImage?
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(CGRect(origin: .zero, size: size))
+        img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        if let img = img {
+            return img
+        }
+        return img ?? UIImage()
+    }
+    
+    /// 寻找podspec中颜色资源
     internal class func readColor(colorName: String) -> UIColor {
         let bundle = AHTools.findSourceBundle()
         return UIColor(named: colorName, in: bundle, compatibleWith: nil) ?? UIColor()
